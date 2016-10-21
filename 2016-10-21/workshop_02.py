a0cd0eb1f51eeb77000f65f2e08b3d3c8e61e692
Carlo Loffredo <carlettof@yahoo.it>
2016-10-21 17:06:30 +0200

    assignment completed

--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -3,84 +3,94 @@ from random import randint
 
 
 def frame(bx, bz, px, py, beam_Y, pillar_Z):
-	beamNumb = 9		# number of beams
-	#pillarsNumb = 10	# number of pillars
 
 	beamX = bx		# x dimension of beam section
 	beamZ = bz		# z dimension of beam section 
 	beamY = beam_Y		# distances between axes of the pillars
-	beams = []		#list of beams
-
+	
 	pillarX = px		# x dimension of pillar section
 	pillarY = py		# y dimension of pillar section
 	pillarZ = pillar_Z	# interstory heights
-	pillars = []
 
-	"""
-	CODE NOT WORKING CORRECTLY
-	
-	# calculate pillar axis distance from (0,0)
-	pillarDistance = [beamY[0]]
-	for i in range(1,len(beamY)):
-		pillarDistance.append(sum(beamY[0:i+1]))
-
-	# calculate beam axis distance from (0,0)
-	beamDistance = [pillarZ[0]]
-	for j in range(1,len(beamY)):
-		beamDistance.append(sum(pillarZ[0:j+1]))
-
-	# building all the beam types
-	for n in range(0,len(beamY)):
-		beam = CUBOID([beamX,beamY[n],beamZ])
-		beams.append(beam)
-
-
-
-	# building all the pillar types
-	for p in range(0,len(pillarZ)):
-		pillar = CUBOID([pillarX,pillarY,pillarZ[p]])
-		pillars.append(pillar)
-
-	#for n in range(0,len(pillarZ)):
-	#	beams.append(CUBOID([beamX,beamY[n],beamZ])) 
-	
-
-
-	scene = STRUCT([CUBOID([1,1,1])])
-	pillarDistance = 0
-	beamDistance = 0
-	# building pillars and beams
-
-	for n in range(0,len(pillars)):
-		for m in range(0,len(beams)-1):
-			pillar
-			scene = STRUCT([scene,T(2)(pillarDistance[m]),T(3)((beamDistance[n])-pillarZ[n]),pillars[n]])
-			scene = STRUCT([scene,T(2)(pillarDistance[m]),T(3)(beamDistance[n]),beams[m+1]])
-
-		scene = STRUCT([scene,T(2)(pillarDistance[len(beams)-1]),T(3)((beamDistance[n])-pillarZ[n]),pillars[n]])
-	"""
+	frame = STRUCT([CUBOID([0.01,0.01,0.01])])
+	height = 0
 	
 	# First, building the pillars
 	
-	a = []
+	for h in range(0,len(pillarZ)):
+		a = []
 	
-	for i in range(0,len(beamY)):
+		for i in range(0,len(beamY)):
+			a.append(py)
+			a.append(-beamY[i]+py)
 		a.append(py)
-		a.append(-beamY[i]+py)
-	a.append(py)
 	
-	A = QUOTE(a)
-	B = QUOTE([px])
-	C = PROD([A,B])
-	H = QUOTE([pillarZ[0]])
-	D = PROD([C,H])
+		A = QUOTE(a)
+		B = QUOTE([px])
+		C = PROD([A,B])
+		H = QUOTE([pillarZ[h]])
+		D = PROD([C,H])
+		frame = STRUCT([frame,T(3)(height),D])
+		height = height + pillarZ[h] + bz
 	
 	
 	# Then, building the beams
 	
-
+	height = 0
 	
-	return D
+	for h in range(0,len(pillarZ)):
+		a = []
+		height = height + pillarZ[h]
+		for i in range(0,len(beamY)):
+			if (i == len(beamY) - 1):
+				a.append(beamY[i] + py)
+			else:
+				a.append(beamY[i])
+	
+		A = QUOTE(a)
+		B = QUOTE([bx])
+		C = PROD([A,B])
+		H = QUOTE([bz])
+		D = PROD([C,H])
+		frame = STRUCT([frame,T(3)(height),D])
+		height = height + bz
+	
+	return frame
+	
+def beams(frameParams):
+	
+	beamX = frameParams[1][0]		# x dimension of beam section
+	beamZ = frameParams[1][1]		# z dimension of beam section 
+	beamY = frameParams[2]			# distances between axes of the pillars
+	
+	pillarY = frameParams[1][3]
+	pillarZ = frameParams[3]		# interstory heights
+	height = 0
+	
+	newBeamLenght = frameParams[0][0]
+	beams = STRUCT([CUBOID([0.01,0.01,0.01])])
+	
+	# Build the interconnecting beams
+	
+	for h in range(0,len(pillarZ)):
+		a = []
+		height = height + pillarZ[h]
+		for i in range(0,len(beamY)):
+			a.append(pillarY)
+			a.append(-beamY[i]+pillarY)
+		a.append(pillarY)
+		
+		A = QUOTE(a)
+		B = QUOTE([newBeamLenght])
+		C = PROD([A,B])
+		H = QUOTE([beamZ])
+		D = PROD([C,H])
+		
+		beams = STRUCT([beams,T(3)(height),D])
+		height = height + beamZ
+	
+	return beams
+	
 	
 
 def ggpl_bone_structure(file_name):
@@ -126,23 +136,35 @@ def ggpl_bone_structure(file_name):
 	
 	#scene = frame(listFrames[0][1][0],listFrames[0][1][1],listFrames[0][1][2],listFrames[0][1][3],listFrames[0][3],listFrames[0][2])
 
-	scene = STRUCT([CUBOID([1,1,1])])
+	scene = STRUCT([CUBOID([0.001,0.001,0.001])])
 	
 	distance = 0
 	
 	# now building placing frames in scene
+	
 	for i in range(0,len(listFrames)):
 		# update the vector
 		distance = distance + listFrames[i][0][0]
 		# build new frame
-		# TODO: function frame() does not work correctly (yet)
-		newframe = frame(listFrames[0][1][0],listFrames[0][1][1],listFrames[0][1][2],listFrames[0][1][3],listFrames[0][3],listFrames[0][2])
+		newFrame = frame(listFrames[i][1][0],listFrames[i][1][1],listFrames[i][1][2],listFrames[i][1][3],listFrames[i][2],listFrames[i][3])
 		# place new frame in scene
-		scene = STRUCT([scene,T(2)(distance),newframe])
+		scene = STRUCT([scene,T(2)(distance),newFrame])
+	
+	
+	
+	distance = listFrames[0][1][2]
 	
 	# now building and placing connecting beams
-	# TODO
+	for i in range(0,len(listFrames)):
+		# build the connecting beams structure
+		newBeams = beams(listFrames[i])
+		# place new connecting beams in scene
+		scene = STRUCT([scene,T(2)(distance),newBeams])
+		# update the vector
+		distance = distance + listFrames[i][0][0]
 	
+	
+	#return scene
 	return scene
 
 VIEW(ggpl_bone_structure("frame_data_000000.csv"))
